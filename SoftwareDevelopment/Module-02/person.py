872447e6729f35993526a33a396604a3df477d26
Rexouium <mitch.brederije@gmail.com>
2022-09-15 14:19:48 +0200

    Epic-Opdracht

---
+++ SoftwareDevelopment/Module-02/person.py
@@ -0,0 +1,16 @@
+from codecs import namereplace_errors
+
+
+naam = str(input("Wat is uw naam"))
+adres = str(input("Welk huis nummer"))
+postcode = str(input("Wat is de postcode"))
+woonplaats = str(input("Waar woon je"))
+
+
+
+print("--------------------------")
+print("|   Naam:",naam)
+print("|   Adres:",adres)
+print("|   Postcode:",postcode)
+print("|   Woonplaats:",woonplaats)
+print("--------------------------")
