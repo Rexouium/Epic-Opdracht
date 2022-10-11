a3cbcb6dc393499ecd98285449b7963bee07ada8
Rexouium <mitch.brederije@gmail.com>
2022-10-11 15:17:29 +0200

    yes

---
+++ SoftwareDevelopment/Module-02/Cladblaadje-python.py
@@ -0,0 +1,25 @@
+# Leeftijd = int(input("Wat is je leeftijd"))
+# Rijbewijs = input("Heb je een rijbewijs J/N")
+
+# if Leeftijd >= 16 and Leeftijd < 18 and Rijbewijs == "J":
+#     AnderPersoon = input("Zit er iemand naast je? J/N")
+#     if AnderPersoon == "N":
+#         print("Je mag geen auto rijden")
+#     elif AnderPersoon == "J":
+#         print("Je mag autorijden")
+
+# elif Leeftijd >= 18 and Rijbewijs == "J":
+#     print("Je mag autorijden")
+
+
+# zin = "Hallo wereld"
+# for c in zin:
+#     print(c)
+
+# for item in (2,7,9,3):
+#     print(item)
+
+
+HealthPoints = 200
+
+damage = 7.5
