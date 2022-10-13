dd27adb77ca102486c7a14cdad7e2dd3a7f5ecc9
Rexouium <mitch.brederije@gmail.com>
2022-10-13 10:07:37 +0200

    yes

--- SoftwareDevelopment/Module-02/Zwembad.py
+++ SoftwareDevelopment/Module-02/Zwembad.py
@@ -27,9 +27,41 @@ elif RijAfstand >= 50 and GroteZwembad >= 20:
     PrijsPerKilometers = 2.05
     EindPrijsKilometers = (RijAfstand * PrijsPerKilometers)
 ###########################################################################
+#Tegels berekenen
+###########################################################################
+TegelKleur = input("Normaal, Rood, NaarKeuze").lower()
+
+TegelPrijsPerm2 = 250
+if TegelKleur == "normaal":
+    if GroteZwembad < 20:
+        TegelPrijsPerm2 = 250
+    elif GroteZwembad >= 20:
+        TegelPrijsPerm2 = 200
+
+elif TegelKleur == "rood":
+    if GroteZwembad < 20:
+        TegelPrijsPerm2 = 275
+    elif GroteZwembad >= 20:
+        TegelPrijsPerm2 = 220
+
+elif TegelKleur == "naarkeuze":
+    if GroteZwembad < 20:
+        TegelPrijsPerm2 = 350
+    elif GroteZwembad >= 20:
+        TegelPrijsPerm2 = 325
+
+
 
 HoeveelGrond = Lengte * Breedte * Hoogte
-print("Uitgraaf kosten =", UitGraafKosten * GroteZwembad, "Euro")
-print("Afvoeren Grond =", HoeveelGrond * AfgevoerdeGrondPrijs, "Euro")
+
+AfgevoerdeGrondTotaalPrijs = HoeveelGrond * AfgevoerdeGrondPrijs
+UitGraafKostenPrijs = UitGraafKosten * GroteZwembad
+TegelTotaalPrijs = TegelPrijsPerm2 * (Lengte * Breedte)
+TotaalPrijs = (EindPrijsKilometers + UitGraafKostenPrijs + TegelTotaalPrijs + AfgevoerdeGrondTotaalPrijs)
+print(" ")
+print(" ")
+print("Uitgraaf kosten =", UitGraafKostenPrijs, "Euro")
+print("Afvoeren Grond =", AfgevoerdeGrondTotaalPrijs, "Euro")
 print("Voor rij kosten =", EindPrijsKilometers, "Euro")
-print("Totaal =", EindPrijsKilometers + HoeveelGrond, "Euro")
+print("Beton + Tegel kosten =", TegelTotaalPrijs)
+print("Totaal =", TotaalPrijs, "Euro")
