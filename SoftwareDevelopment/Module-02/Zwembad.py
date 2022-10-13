058f40dc0d50347f51e1f2406dcb3f27cdea56be
Rexouium <mitch.brederije@gmail.com>
2022-10-13 09:48:23 +0200

    yes

--- SoftwareDevelopment/Module-02/Zwembad.py
+++ SoftwareDevelopment/Module-02/Zwembad.py
@@ -5,7 +5,7 @@ Hoogte = float(input("HoeHoog"))
 Breedte = float(input("HoeBreed"))
 
 VastePrijs = 0
-PrijsPerKilometers = 0
+EindPrijsKilometers = 0
 UitGraafKosten = 25
 AfgevoerdeGrondPrijs =32.50
 
@@ -13,20 +13,22 @@ AfgevoerdeGrondPrijs =32.50
 if RijAfstand < 50 and GroteZwembad < 20:
     VastePrijs = 100
     PrijsPerKilometers = 1.25
-    PrijsPerKilometers += (RijAfstand * PrijsPerKilometers)
+    EindPrijsKilometers = (RijAfstand * PrijsPerKilometers)
 elif RijAfstand >= 50 and GroteZwembad < 20:
     VastePrijs = 100
     PrijsPerKilometers = 1.15
-    PrijsPerKilometers += (RijAfstand * PrijsPerKilometers)
+    EindPrijsKilometers = (RijAfstand * PrijsPerKilometers)
 elif RijAfstand < 50 and GroteZwembad >= 20:
     VastePrijs = 250
     PrijsPerKilometers = 2.15
-    PrijsPerKilometers += (RijAfstand * PrijsPerKilometers)
+    EindPrijsKilometers = (RijAfstand * PrijsPerKilometers)
 elif RijAfstand >= 50 and GroteZwembad >= 20:
     VastePrijs = 250
     PrijsPerKilometers = 2.05
-    PrijsPerKilometers += (RijAfstand * PrijsPerKilometers)
+    EindPrijsKilometers = (RijAfstand * PrijsPerKilometers)
 ###########################################################################
 
 HoeveelGrond = Lengte * Breedte * Hoogte
-print("Hoeveel grond =", HoeveelGrond, "M3" )#AfgevoerdeGrondPrijs, "Euro")
+print("Afvoeren Grond =", HoeveelGrond * AfgevoerdeGrondPrijs, "Euro")
+print("Voor rij kosten =", EindPrijsKilometers, "Euro")
+print("Totaal =", EindPrijsKilometers + HoeveelGrond, "Euro")
