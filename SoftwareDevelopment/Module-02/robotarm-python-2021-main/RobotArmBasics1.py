d45ec7c0bf549bf37775c4e342a7097b8dfff766
Rexouium <mitch.brederije@gmail.com>
2022-10-19 10:06:54 +0200

    opdracht 1

--- SoftwareDevelopment/Module-02/robotarm-python-2021-main/RobotArmBasics1.py
+++ SoftwareDevelopment/Module-02/robotarm-python-2021-main/RobotArmBasics1.py
@@ -1,5 +1,4 @@
 # Robotarm bibliotheek inladen
-from shutil import move
 from RobotArm import RobotArm
             
 # De robotarm opstarten in een bepaald level, bijvoorbeeld 'exercise 1'
@@ -8,13 +7,7 @@ robotArm = RobotArm('exercise 1')
 # Jouw python instructies zet je vanaf hier:
 robotArm.moveRight()
 robotArm.grab()
-MoveR1 = 1
-while MoveR1 < 6:
-    robotArm.moveRight()
-    if MoveR1 > 5:
-        break
-    else:
-        MoveR1 += 1
+robotArm.moveLeft()
 robotArm.drop()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
