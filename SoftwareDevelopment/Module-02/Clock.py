45e7aceebad195deea5838aa66e05f90f897e8c0
Rexouium <mitch.brederije@gmail.com>
2022-10-21 09:21:15 +0200

    yessnt

--- SoftwareDevelopment/Module-02/Clock.py
+++ SoftwareDevelopment/Module-02/Clock.py
@@ -1,9 +1,5 @@
-Time = 0
-
 for i in range(0,25):
     if i < 13:
         print(i,":00 AM")
-        Time += 1
     elif i < 25 and i > 12:
         print(i, ":00 PM")
-        i += 1
--- SoftwareDevelopment/Module-02/Numbersforloop.py
+++ SoftwareDevelopment/Module-02/Numbersforloop.py
@@ -1,11 +1,2 @@
 for i in range(20,52 , 2):
-    print("")
-
-
-Test = 1
-while Test == 1:
-    print("Joe mama")
-    break
-
-for i in Test == 1:
-    print("RINDKFMDN")
+    print(i)
