28626910fd4e8773c10e857d9537f60192f4b710
Rexouium <mitch.brederije@gmail.com>
2022-12-13 13:42:07 +0100

    Kaarten

---
+++ SoftwareDevelopment/Module-04/Kaarten.py
@@ -0,0 +1,14 @@
+import random
+Kleuren = ("Harten", "Klaveren", "Schoppen", "Ruiten")
+Types = ("2","3","4","5","6","7","8","9","10", "Boer", "Vrouw", "Heer", "aas")
+Deck = []
+for x in range(4):
+    NulValue = 0
+    for h in range(13):
+        Deck.append(Kleuren[x] +" "+ Types[h])
+    Deck.append("Joker") if x < 2 else None
+random.shuffle(Deck)
+for g in range(7):
+    print(f"Kaart {g + 1}: "+ Deck[x])
+    Deck.remove(Deck[x])
+print(f"\ndeck ({len(Deck)} kaarten): {', '.join(Deck)}")
