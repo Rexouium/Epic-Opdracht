71f8913d516e8babf44f65c9b370101e16ee1181
Rexouium <mitch.brederije@gmail.com>
2022-12-13 15:00:30 +0100

    M&M YES

--- SoftwareDevelopment/Module-04/MenM.py
+++ SoftwareDevelopment/Module-04/MenM.py
@@ -1,20 +1,15 @@
 import random
-#M&M's Systems
-LegeList = []
-KleurLijst = ("oranje", "blauw", "groen", "bruin")
-MenMAantal = int(input("Hoe veel m en m's:  "))
-
-kleurAantal = len(KleurLijst)
-
-print(kleurAantal)
-
-WhileValue = True
-
-while WhileValue:
-    ChoosingMENM = random.choice(KleurLijst)
-    LegeList.append(ChoosingMENM)
-    if len(LegeList) == MenMAantal:
-        WhileValue = False
-
-
-print(f"In de zak zitten de volgende emenems: {', '.join(LegeList)}")
+while not (aantal := input("Hoe veel m&m's?: ")).isdigit():
+    print("Getal aub")
+KleurLijst = ["rood", "blauw", "groen", "geel", "bruin"]
+MenM = {}
+for x in range(int(aantal)):
+    Keuze = random.choice(KleurLijst)
+    print(Keuze)
+    if Keuze in MenM:
+        MenM[Keuze] += 1
+        print("Updating Kleur")
+    else:
+        print("Kleur Toevoegen")
+        MenM[Keuze] = 1
+print(MenM)
