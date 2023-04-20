8aa2d39c6d144f11e2af660ad36c66211ac29a0e
Rexouium <mitch.brederije@gmail.com>
2023-04-20 10:22:31 +0200

    Opdracht 1 tot 3 comit

---
+++ SoftwareDevelopment/AVI score 20-04-2023/program.py
@@ -0,0 +1,33 @@
+from functies import *
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox  # if you want to send messages to the user.
+
+# functions
+
+
+def calculate():
+    textToBeCalculated = calculateInput.get('1.0', 'end') # get all the text from inputfield
+    characterLabel.config(text=f"Characters: {getNumberOfCharacters(textToBeCalculated)}")
+    sentencesLabel.config(text=f"Zinnen: {getNumberOfSentences(textToBeCalculated)}")
+    woordenLabel.config(text=f"Woorden: {getNumberOfWords(textToBeCalculated)}")
+
+
+# variables TK
+root = tk.Tk()              # create tkInter window
+root.title('Text analyser')  # set title
+root.geometry('600x600')    # set dimension
+calculateInput = tk.Text(root, width = 70, height = 30, background='lightgrey')            # generate imput element
+calculateButton = ttk.Button(root, text='Bereken score(s)', command=calculate)  #generate button when pressed -> calculate
+characterLabel = tk.Label(root, text=f'Karakters:', width=20, bg='black', fg='white')  # generate characterLabel
+sentencesLabel = tk.Label(root, text=f'Zinnen:', width=20, bg='black', fg='white')     # generate sentencesLabel
+woordenLabel = tk.Label(root, text=f'Woorden:', width=20, bg='black', fg='white')     # generate sentencesLabel
+
+calculateInput.place(x=20, y=20)   # place is one of the ways to put elements on root (window).
+calculateButton.place(x=20, y=520)
+characterLabel.place(x=180, y=560)
+sentencesLabel.place(x=20, y=560)
+woordenLabel.place(x=20, y=600)
+
+# start program
+root.mainloop()  # runs until stopped with default stop button.
